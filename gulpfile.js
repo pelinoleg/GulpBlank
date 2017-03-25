e5065de1ef12b138e592b2e9a670386737fbe778
Pelin Oleg <oleg@MacBook-Pro-Oleg.local>
2017-03-25 18:04:50 +0200

    js format

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
-var	stylus = require('gulp-stylus');
-var	browserSync = require('browser-sync');
-var	pug = require('gulp-pug');
+var stylus = require('gulp-stylus');
+var browserSync = require('browser-sync');
+var pug = require('gulp-pug');
 var notify = require("gulp-notify");
 var autoprefixer = require('gulp-autoprefixer');
 var SourceMap = require('gulp-sourcemaps');
@@ -20,40 +20,47 @@ var zip = require('gulp-zip');
 
 
 // Compile stylus,add prefixes, minify css, reload browser and notify
-gulp.task( 'stylus', function(){
+gulp.task('stylus', function() {
   return gulp.src('app/stylus/*.styl') // source folder
-  .pipe(SourceMap.init())
-  .pipe( stylus().on( 'error', notify.onError({
+    .pipe(SourceMap.init())
+    .pipe(stylus().on('error', notify.onError({
       message: "<%= error.message %>",
-      title  : "Stylus Error!"
-    }))
-	) // error notification
-	.pipe(autoprefixer(['last 10 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // add prefix
-	.pipe(rename('style.css'))
-	.pipe(SourceMap.write('.')) //Create source map
-  .pipe(gulp.dest('app/css')) //destination folder
-	.pipe(browserSync.reload({stream: true})) // browser reload styles
-  // .pipe(notify('Stylus compiled')); // uncomment this line for "success" notify
+      title: "Stylus Error!"
+    }))) // error notification
+    .pipe(autoprefixer(['last 10 versions', '> 1%', 'ie 8', 'ie 7'], {
+      cascade: true
+    })) // add prefix
+    .pipe(rename('style.css'))
+    .pipe(SourceMap.write('.')) //Create source map
+    .pipe(gulp.dest('app/css')) //destination folder
+    .pipe(browserSync.reload({
+      stream: true
+    })) // browser reload styles
+    // .pipe(notify('Stylus compiled')); // uncomment this line for "success" notify
 });
 
 
 // Компилируем Pug в HTML и перегружаем страницу
-gulp.task('pug', function(){
-	return gulp.src('app/pug/*.pug') // source folder
-	.pipe(pug({pretty: true})) //compile pug
-	.pipe(gulp.dest('app')) // destination folder
-	.pipe(browserSync.reload({stream: true}) ) // browser refresh
+gulp.task('pug', function() {
+  return gulp.src('app/pug/*.pug') // source folder
+    .pipe(pug({
+      pretty: true
+    })) //compile pug
+    .pipe(gulp.dest('app')) // destination folder
+    .pipe(browserSync.reload({
+      stream: true
+    })) // browser refresh
 });
 
 
 // подключаем стили и скрипты автоматом котоорые были установлены через bower
 // bower i --save magnific-popup
 // -save для добавления в dependencies (очень важно)
-gulp.task('bower', function () {
+gulp.task('bower', function() {
   gulp.src('app/pug/includes/_head.pug')
     .pipe(wiredep({
       directory: "app/libs",
-			'ignorePath': '../../'
+      'ignorePath': '../../'
     }))
     .pipe(gulp.dest('app/pug/includes'));
 });
@@ -64,30 +71,32 @@ gulp.task('bower', function () {
 gulp.task('htmlbeautify', function() {
   gulp.src('app/*.html')
     .pipe(htmlbeautify({
-		    "indent_size": 4,
-		}))
+      "indent_size": 4,
+    }))
     .pipe(gulp.dest('app'))
 });
 
 
 // refresh browser
-gulp.task('browser-sync', function(){
-	browserSync({
-		server:{
-			baseDir: 'app'
-		},
-		notify: false
-	})
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+    notify: false
+  })
 });
 
 //delete "dist" map
 gulp.task('clean', function() {
-    return gulp.src('dist', {read: false})
-        .pipe(clean());
+  return gulp.src('dist', {
+      read: false
+    })
+    .pipe(clean());
 });
 
 //compress image and copy to dist/img
-gulp.task('image', function () {
+gulp.task('image', function() {
   return gulp.src('app/img/*')
     .pipe(cache(image()))
     .pipe(gulp.dest('dist/img'));
@@ -95,31 +104,33 @@ gulp.task('image', function () {
 
 // copy fonts to dist
 gulp.task('copyfonts', function() {
-   return gulp.src('app/fonts//**/*.*')
-   .pipe(gulp.dest('dist/fonts'));
+  return gulp.src('app/fonts//**/*.*')
+    .pipe(gulp.dest('dist/fonts'));
 });
 
 // create archive for build (map "dist")
 gulp.task('zip', () =>
-    gulp.src('dist/**/*')
-        .pipe(zip('build.zip'))
-        .pipe(gulp.dest('./'))
+  gulp.src('dist/**/*')
+  .pipe(zip('build.zip'))
+  .pipe(gulp.dest('./'))
 );
 
 //запускаем все команды чтоб прошлись по файлам, потом мониторим все изменения.
-gulp.task('default', ['stylus', 'pug', 'bower', 'htmlbeautify', 'browser-sync'], function() {
-  gulp.watch('app/stylus/**/*.styl', ['stylus']); //watch stylus files
-  gulp.watch('app/pug/**/*.pug', ['pug']); //watch pug files
-  gulp.watch('bower.json', ['bower']); //watch if install scripts with bower
-  gulp.watch('app/*.html', ['htmlbeautify']); //watch html files and beautify
-});
+gulp.task('default', ['stylus', 'pug', 'bower', 'htmlbeautify', 'browser-sync'],
+  function() {
+    gulp.watch('app/stylus/**/*.styl', ['stylus']); //watch stylus files
+    gulp.watch('app/pug/**/*.pug', ['pug']); //watch pug files
+    gulp.watch('bower.json', ['bower']); //watch if install scripts with bower
+    gulp.watch('app/*.html', ['htmlbeautify']); //watch html files and beautify
+  });
 
 //comppile stylus and pug, add bower components in files and beautify html
-gulp.task('compile', gulpsync.sync(['stylus', 'pug', 'bower', 'htmlbeautify']), function() {});
+gulp.task('compile', gulpsync.sync(['stylus', 'pug', 'bower', 'htmlbeautify']),
+  function() {});
 
 
 // build project in "dist map"
-gulp.task('build', gulpsync.sync(['clean', 'image', 'copyfonts']), function () {
+gulp.task('build', gulpsync.sync(['clean', 'image', 'copyfonts']), function() {
   return gulp.src('app/*.html')
     .pipe(useref())
     .pipe(gulpif('*.js', uglify()))
